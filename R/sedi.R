5ddf4ef11ed57d6c5d4b1c8bb143af56a8f039df
RWunderlich <rainer.wunderlich@gmail.com>
2018-03-19 09:52:19 +0800

    Update sedi.R
    catching division by zero by approximating zeros in divisions as 1e-9

--- R/sedi.R
+++ R/sedi.R
@@ -1,11 +1,33 @@
-# R functions to compute SEDI and it 95% CI
+# R functions to compute SEDI and its 95% CI
 # by Rainer Wunderlich
-# based on Ferro, C.A.T. & Stephensons, D.B. (2011), Weather & Forecasting, 26, 699-713
+# based on Ferro, C.A.T. & Stephensons, D.B. (2011),
+# Extremal Dependence Indices: Improved Verification Measures for Deterministic Forecasts of Rare Binary Events,
+# Weather & Forecasting, 26, 699-713
+# DOI:10.1175/WAF-D-10-05030.1
+
+# PLEASE NOTE:
+# To avoid undefined values, I substitute terms resulting in a division by zero with a division by 1e-9
 
 # helper
 # hit rate
 HitRate <- function(a, c) {
   return (a / (a + c))
+}
+
+OneMHR <- function(a, c) {
+  if (HitRate(a, c) == 1) {
+    return(1e-9)
+  }
+  else
+  return(1 - (a / (a + c)))
+}
+
+logHR <- function(a, c) {
+  if (HitRate(a, c) == 0) {
+    return(log(1e-9))
+  }
+  else
+    return(log(HitRate(a, c)))
 }
 
 # helper
@@ -14,33 +36,50 @@ FalseAlarm <- function(b, d) {
   return (b / (b + d))
 }
 
+OneMFA <- function(b, d) {
+  if (FalseAlarm(b, d) == 1) {
+    return(1e-9)
+  }
+  else
+  return (1 - (b / (b + d)))
+}
+
+logFA <- function(b, d) {
+  if (FalseAlarm(b, d) == 0) {
+    return(log(1e-9))
+  }
+  else
+    return(log(FalseAlarm(b, d)))
+}
+
+
 # helper
 # odds ratio
 OddsRatio <- function(a, b, c, d) {
   return (
-          (HitRate(a, c) * (1 - FalseAlarm(b, d)))
+          (HitRate(a, c) * (OneMFA(b, d)))
           /
-            (FalseAlarm(b, d) * (1 - HitRate(a, c)))
+            (FalseAlarm(b, d) * (OneMHR(a, c)))
           )
 }
 
 # helper
 # denominator CI
 CIdeno <- function(a, b, c, d) {
-  return ( 2 * abs( (((1 - HitRate(a, c)) * (1 - FalseAlarm(b, d)) + HitRate(a, c) * FalseAlarm(b, d))/((1 - HitRate(a, c)) * (1 - FalseAlarm(b, d)))) *
-                      (log( FalseAlarm(b, d) * (1 - HitRate(a, c)))) + ((2 * HitRate(a, c)) / (1 - HitRate(a, c))) * (log(HitRate(a, c) * (1 - FalseAlarm(b, d))))  )  )
+  return ( 2 * abs( (((OneMHR(a, c)) * (OneMFA(b, d)) + HitRate(a, c) * FalseAlarm(b, d))/((OneMHR(a, c)) * (OneMFA(b, d)))) *
+                      (log( FalseAlarm(b, d) * (OneMHR(a, c)))) + ((2 * HitRate(a, c)) / (OneMHR(a, c))) * (log(HitRate(a, c) * (OneMFA(b, d))))  )  )
 }
 
 # helper
 # divisor CI
 CIdivi <- function(a, b, c, d) {
-  return (HitRate(a, c) * (log(FalseAlarm(b, d) * (1 - HitRate(a, c)))   +  log(HitRate(a, c) * (1 - FalseAlarm(b, d))))^2)
+  return (HitRate(a, c) * (log(FalseAlarm(b, d) * (OneMHR(a, c)))   +  log(HitRate(a, c) * (OneMFA(b, d))))^2)
 }
 
 # helper
 # trailing sq term
 CIsqtail <- function(a, b, c, d) {
-  return (sqrt((HitRate(a, c) * (1 - HitRate(a, c))) / (a + c)))
+  return (sqrt((HitRate(a, c) * (OneMHR(a, c))) / (a + c)))
 }
 
 
@@ -49,9 +88,9 @@ CIsqtail <- function(a, b, c, d) {
 # symmetric extremal dependence index
 sedi <- function(a, b, c, d) {
   return (
-          (log(FalseAlarm(b, d)) - log(HitRate(a, c)) - log(1 - FalseAlarm(b, d)) + log(1 - HitRate(a, c)))
+          (logFA(b, d) - logHR(a, c) - log(OneMFA(b, d)) + log(OneMHR(a, c)))
           /
-          (log(FalseAlarm(b, d)) + log(HitRate(a, c)) + log(1 - FalseAlarm(b, d)) + log(1 - HitRate(a, c)))
+          (logFA(b, d) + logHR(a, c) + log(OneMFA(b, d)) + log(OneMHR(a, c)))
           )
 }
 
