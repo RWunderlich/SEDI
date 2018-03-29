2016170819d8765232fc7a21d337782c927e5e39
RWunderlich <rainer.wunderlich@gmail.com>
2018-03-29 17:08:40 +0800

    Update sedi.R
    minor fixes

--- R/sedi.R
+++ R/sedi.R
@@ -6,120 +6,129 @@
 # DOI:10.1175/WAF-D-10-05030.1
 
 # PLEASE NOTE:
-# To avoid undefined values, I substitute some terms/values with -1e+09 or 1e-09
+# To avoid undefined values, I substitute some terms/values with -1e+09 * (a + b + c + d) and 1e-09
+
+# Approximations
+minusInf <- function(a = a, b = b, c = c, d = d) {
+    return(-1e+09 * (a + b + c + d))
+}
+
+plusZero <- function(a = a, b = b, c = c, d = d) {
+    return(1e-09)
+}
 
 # helper
 # base rate
-BaseRate <- function(a, b, c, d) {
+BaseRate <- function(a = a, b = b, c = c, d = d) {
   return ((a + c) / (a + b + c + d))
 }
 
 # helper
 # hit rate
-HitRate <- function(a, c) {
+HitRate <- function(a = a, b = b, c = c, d = d) {
   return (a / (a + c))
 }
 
-OneMHR <- function(a, c) {
-  if (isTRUE(HitRate(a, c) == 1)) {
-    return(1e-09)
+OneMHR <- function(a = a, b = b, c = c, d = d) {
+  if (isTRUE(HitRate(a = a, b = b, c = c, d = d) == 1)) {
+    return(plusZero(a = a, b = b, c = c, d = d))
   }
   else
   return(1 - (a / (a + c)))
 }
 
-logHR <- function(a, c) {
-  if (isTRUE(HitRate(a, c) == 0)) {
-    return(-1e+09)
+logHR <- function(a = a, b = b, c = c, d = d) {
+  if (isTRUE(HitRate(a = a, b = b, c = c, d = d) == 0)) {
+    return(minusInf(a = a, b = b, c = c, d = d))
   }
   else
-    return(log(HitRate(a, c)))
+    return(log(HitRate(a = a, b = b, c = c, d = d)))
 }
 
 # helper
 # false alarm
-FalseAlarm <- function(b, d) {
+FalseAlarm <- function(a = a, b = b, c = c, d = d) {
   return (b / (b + d))
 }
 
-OneMFA <- function(b, d) {
-  if (isTRUE(FalseAlarm(b, d) == 1)) {
-    return(1e-09)
+OneMFA <- function(a = a, b = b, c = c, d = d) {
+  if (isTRUE(FalseAlarm(a = a, b = b, c = c, d = d) == 1)) {
+    return(plusZero(a = a, b = b, c = c, d = d))
   }
   else
   return (1 - (b / (b + d)))
 }
 
-logFA <- function(b, d) {
-  if (isTRUE(FalseAlarm(b, d) == 0)) {
-    return(-1e+09)
+logFA <- function(a = a, b = b, c = c, d = d) {
+  if (isTRUE(FalseAlarm(a = a, b = b, c = c, d = d) == 0)) {
+    return(minusInf(a = a, b = b, c = c, d = d))
   }
   else
-    return(log(FalseAlarm(b, d)))
+    return(log(FalseAlarm(a = a, b = b, c = c, d = d)))
 }
 
 # not required but useful
 # odds ratio
-OddsRatio <- function(a, b, c, d) {
+OddsRatio <- function(a = a, b = b, c = c, d = d) {
   return (
-          (HitRate(a, c) * (OneMFA(b, d)))
+          (HitRate(a = a, b = b, c = c, d = d) * (OneMFA(a = a, b = b, c = c, d = d)))
           /
-            (FalseAlarm(b, d) * (OneMHR(a, c)))
+            (FalseAlarm(a = a, b = b, c = c, d = d) * (OneMHR(a = a, b = b, c = c, d = d)))
           )
 }
 
 # not required but useful
 # ORSS
-ORSS <- function(a, b, c, d) {
-  return ((HitRate(a = a, c = c) - FalseAlarm(b = b, d = d))
+ORSS <- function(a = a, b = b, c = c, d = d) {
+  return ((HitRate(a = a, b = b, c = c, d = d) - FalseAlarm(a = a, b = b, c = c, d = d))
           /
-          (HitRate(a = a, c = c) + FalseAlarm(b = b, d = d) - 2*(HitRate(a = a, c = c)*FalseAlarm(b = b, d = d))))
+          (HitRate(a = a, b = b, c = c, d = d) + FalseAlarm(a = a, b = b, c = c, d = d) - 2*(HitRate(a = a, b = b, c = c, d = d)*FalseAlarm(a = a, b = b, c = c, d = d))))
 }
 
 # not required but useful
 # TSS
-TSS <- function(a, b, c, d) {
-  return (HitRate(a = a, c = c) - FalseAlarm(b = b, d = d))
+TSS <- function(a = a, b = b, c = c, d = d) {
+  return (HitRate(a = a, b = b, c = c, d = d) - FalseAlarm(a = a, b = b, c = c, d = d))
 }
 
 # helper
 # denominator CI
-CIdeno <- function(a, b, c, d) {
-  return ( 2 * abs( (((OneMHR(a, c)) * (OneMFA(b, d)) + HitRate(a, c) * FalseAlarm(b, d))/((OneMHR(a, c)) * (OneMFA(b, d)))) *
-                      (log( FalseAlarm(b, d) * (OneMHR(a, c)))) + ((2 * HitRate(a, c)) / (OneMHR(a, c))) * (log(HitRate(a, c) * (OneMFA(b, d))))  )  )
+CIdeno <- function(a = a, b = b, c = c, d = d) {
+  return ( 2 * abs( (((OneMHR(a = a, b = b, c = c, d = d)) * (OneMFA(a = a, b = b, c = c, d = d)) + HitRate(a = a, b = b, c = c, d = d) * FalseAlarm(a = a, b = b, c = c, d = d))/((OneMHR(a = a, b = b, c = c, d = d)) * (OneMFA(a = a, b = b, c = c, d = d)))) *
+                      (log( FalseAlarm(a = a, b = b, c = c, d = d) * (OneMHR(a = a, b = b, c = c, d = d)))) + ((2 * HitRate(a = a, b = b, c = c, d = d)) / (OneMHR(a = a, b = b, c = c, d = d))) * (log(HitRate(a = a, b = b, c = c, d = d) * (OneMFA(a = a, b = b, c = c, d = d))))  )  )
 }
 
 # helper
 # divisor CI
-CIdivi <- function(a, b, c, d) {
-  return (HitRate(a, c) * (log(FalseAlarm(b, d) * (OneMHR(a, c)))   +  log(HitRate(a, c) * (OneMFA(b, d))))^2)
+CIdivi <- function(a = a, b = b, c = c, d = d) {
+  return (HitRate(a = a, b = b, c = c, d = d) * (log(FalseAlarm(a = a, b = b, c = c, d = d) * (OneMHR(a = a, b = b, c = c, d = d)))   +  log(HitRate(a = a, b = b, c = c, d = d) * (OneMFA(a = a, b = b, c = c, d = d))))^2)
 }
 
 # helper
 # trailing sq term
-CIsqtail <- function(a, b, c, d) {
-  return (sqrt((HitRate(a, c) * (OneMHR(a, c))) / (a + c)))
+CIsqtail <- function(a = a, b = b, c = c, d = d) {
+  return (sqrt((HitRate(a = a, b = b, c = c, d = d) * (OneMHR(a = a, b = b, c = c, d = d))) / (a + c)))
 }
 
 # main function
 # symmetric extremal dependence index
-sedi <- function(a, b, c, d) {
+sedi <- function(a = a, b = b, c = c, d = d) {
   return (
-          (logFA(b, d) - logHR(a, c) - log(OneMFA(b, d)) + log(OneMHR(a, c)))
+          (logFA(a = a, b = b, c = c, d = d) - logHR(a = a, b = b, c = c, d = d) - log(OneMFA(a = a, b = b, c = c, d = d)) + log(OneMHR(a = a, b = b, c = c, d = d)))
           /
-          (logFA(b, d) + logHR(a, c) + log(OneMFA(b, d)) + log(OneMHR(a, c)))
+          (logFA(a = a, b = b, c = c, d = d) + logHR(a = a, b = b, c = c, d = d) + log(OneMFA(a = a, b = b, c = c, d = d)) + log(OneMHR(a = a, b = b, c = c, d = d)))
           )
 }
 
 # confidence Interval 1
 # SE for SEDI
-sediSE <- function(a, b, c, d) {
-  return ((CIdeno(a = a, b = b, c = c, d = d) / CIdivi(a = a, b = b, c = c, d = d)) * CIsqtail(a = a, c = c) * 1.95996)
+sediSE <- function(a = a, b = b, c = c, d = d) {
+  return ((CIdeno(a = a, b = b, c = c, d = d) / CIdivi(a = a, b = b, c = c, d = d)) * CIsqtail(a = a, b = b, c = c, d = d) * 1.95996)
 }
 
 # confidence Interval 2
 # LCL sedi UCL 95%
-sediCI <- function(a, b, c, d) {
+sediCI <- function(a = a, b = b, c = c, d = d) {
   return (c(sedi(a = a, b = b, c = c, d = d) - sediSE(a = a, b = b, c = c, d = d),
             sedi(a = a, b = b, c = c, d = d),
             sedi(a = a, b = b, c = c, d = d) + sediSE(a = a, b = b, c = c, d = d)))
